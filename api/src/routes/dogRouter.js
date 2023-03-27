2bbf4ed232e7803c7232695b6528697bda93354b
Mark-kus <tignanellimarco@gmail.com>
2023-03-27 17:10:08 -0300

    dog router

---
+++ api/src/routes/dogRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+// Traigo los controladores
+const getAllBreeds = require('../controllers/getAllBreeds');
+const getBreedId = require('../controllers/getBreedId.js');
+const getBreedName = require('../controllers/getBreedName');
+const createDog = require('../controllers/createDog');
+
+const dogRouter = Router();
+
+// Configuro el router '/dogs'
+dogRouter.get('/', getAllBreeds);
+dogRouter.get('/:idRaza', getBreedId);
+dogRouter.get('/name', getBreedName);
+dogRouter.post('/', createDog);
+
+module.exports = dogRouter;
