623be7179121a7707b3f7c670961df6561ece70f
Mark-kus <tignanellimarco@gmail.com>
2023-03-27 17:10:02 -0300

    index router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const dogRouter = require('./dogRouter');
+const temperRouter = require('./temperRouter');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/dogs', dogRouter);
+router.use('/temperaments', temperRouter);
 
 
 module.exports = router;
