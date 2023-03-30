ccdc205c55563ec27d3f3ce6d027a4b1f7ad8a96
Mark-kus <tignanellimarco@gmail.com>
2023-03-30 18:42:47 -0300

    reducer structured

--- client/src/Redux/reducer/reducer.js
+++ client/src/Redux/reducer/reducer.js
@@ -1,13 +1,30 @@
-
+import { GET_ALL_DOGS, GET_ALL_TEMPS, GET_DOG_BY_ID, GET_DOG_BY_NAME, CREATE_DOG } from '../types';
 
 // Seteamos el estado inicial del reducer
 const initialState = {
-
+    allDogs: {},
+    allTemps: {},
+    createdDogs: {},
 }
 
 // Seteamos el reducer
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case GET_ALL_DOGS:
+            return state;
+
+        case GET_ALL_TEMPS:
+            return state;
+
+        case GET_DOG_BY_ID:
+            return state;
+
+        case GET_DOG_BY_NAME:
+            return state;
+
+        case CREATE_DOG:
+            return state;
+
         default:
             return state;
     }
