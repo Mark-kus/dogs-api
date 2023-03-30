b8dc79bf3e8cdb82c97144ca9dc4db15829f8c05
Mark-kus <tignanellimarco@gmail.com>
2023-03-30 18:42:40 -0300

    types setted

---
+++ client/src/Redux/types.js
@@ -0,0 +1,5 @@
+export const GET_DOG_BY_NAME = 'GET_DOG_BY_NAME';
+export const CREATE_DOG = 'CREATE_DOG';
+export const GET_DOG_BY_ID = 'GET_DOG_BY_ID';
+export const GET_ALL_DOGS = "GET_ALL_DOGS";
+export const GET_ALL_TEMPS = 'GET_ALL_TEMPS';
