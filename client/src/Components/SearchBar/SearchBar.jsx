5310fa9e7082bff7244fd6054523654963ad0d1d
Mark-kus <tignanellimarco@gmail.com>
2023-03-31 01:39:17 -0300

    Searchbar

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,11 @@
 // SearchBar: un input de búsqueda para encontrar razas de perros por nombre.
+import styles from './Searchbar.module.css';
 
-export default function Searchbar () {
+export default function Searchbar() {
     return (
         <div>
-        <label htmlFor="raceName">Race name</label>
-        <input id="raceName" type="text" name="raceName" />
+            <label htmlFor="raceName">Race name</label>
+            <input id="raceName" type="text" name="raceName" />
         </div>
     )
 }
