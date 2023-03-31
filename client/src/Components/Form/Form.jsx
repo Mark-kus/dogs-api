c856dd5bac99294ef74526f0c5423f4e8981d6c1
Mark-kus <tignanellimarco@gmail.com>
2023-03-31 01:38:12 -0300

    form

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,9 +1,30 @@
 // Este formulario debe ser controlado completamente con JavaScritp.
-// No se pueden utilizar validaciones HTML, ni utilizar librerías especiales para esto. Debe contar con los siguientes campos:
-
-// Nombre.
-// Altura (diferenciar entre altura mínima y máxima de la raza).
-// Peso (diferenciar entre peso mínimo y máximo de la raza).
-// Años de vida.
 // Posibilidad de seleccionar/agregar varios temperamentos en simultáneo.
-// Botón para crear la nueva raza.
+import styles from './Form.module.css';
+
+export default function Form() {
+    return (
+        <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' />
+
+            <label htmlFor="minHeight">Min-height</label>
+            <input type="text" id='minHeight' />
+            <label htmlFor="maxHeight">Max-height</label>
+            <input type="text" id='maxHeight' />
+
+            <label htmlFor="minWeight">Min-weight</label>
+            <input type="text" id='minWeight' />
+            <label htmlFor="maxWeight">Max-weight</label>
+            <input type="text" id='maxWeight' />
+
+            <label htmlFor="lifespan">Lifespan</label>
+            <input type="text" id='lifespan' />
+
+            <label htmlFor=""></label>
+            <input type="text" id='' />
+
+            <button type='submit'>Submit dog</button>
+        </form>
+    )
+}
