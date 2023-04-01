888c7de1668b4c7192aabc269e9aa7d548b64f26
Mark-kus <tignanellimarco@gmail.com>
2023-04-01 13:17:20 -0300

    Form inputs

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,28 +1,87 @@
 // Este formulario debe ser controlado completamente con JavaScritp.
 // Posibilidad de seleccionar/agregar varios temperamentos en simultáneo.
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Form.module.css';
 
+import validate from './validate.js';
+
 export default function Form() {
+    const { allTemps } = useSelector(state => state)
+    const [inputs, setInputs] = useState({
+        name: '',
+        minHeight: '',
+        maxHeight: '',
+        minWeight: '',
+        maxWeight: '',
+        minLifespan: '',
+        maxLifespan: '',
+        temperaments: '',
+    });
+    const [errors, setErrors] = useState({
+        name: '',
+        minHeight: '',
+        maxHeight: '',
+        minWeight: '',
+        maxWeight: '',
+        minLifespan: '',
+        maxLifespan: '',
+        temperaments: '',
+    });
+
+    const changeHandler = (e) => {
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        })
+        setErrors(validate({
+            ...inputs,
+            [name]: value,
+        }));
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler} className={styles.createForm}>
             <label htmlFor="name">Name</label>
-            <input type="text" id='name' />
+            <input onChange={changeHandler} type="text" id='name' />
+            <p>{errors?.name}</p>
 
-            <label htmlFor="minHeight">Min-height</label>
-            <input type="text" id='minHeight' />
-            <label htmlFor="maxHeight">Max-height</label>
-            <input type="text" id='maxHeight' />
+            <label htmlFor="height">Height</label>
+            <div>
+                <input onChange={changeHandler} name='minHeight' type="number" id='height' />
+                <input onChange={changeHandler} name='maxHeight' type="number" id='height' />
+            </div>
+            <p>{errors?.minHeight || errors?.maxHeight}</p>
 
-            <label htmlFor="minWeight">Min-weight</label>
-            <input type="text" id='minWeight' />
-            <label htmlFor="maxWeight">Max-weight</label>
-            <input type="text" id='maxWeight' />
+            <label htmlFor="weight">Weight</label>
+            <div>
+                <input onChange={changeHandler} name='minWeight' type="number" id='weight' />
+                <input onChange={changeHandler} name='maxWeight' type="number" id='weight' />
+            </div>
+            <p>{errors?.minWeight || errors?.maxWeight}</p>
 
-            <label htmlFor="lifespan">Lifespan</label>
-            <input type="text" id='lifespan' />
+            <label htmlFor="lifespan">Life-span</label>
+            <div>
+                <input onChange={changeHandler} name='minLifespan' type="number" id='lifespan' />
+                <input onChange={changeHandler} name='maxLifespan' type="number" id='lifespan' />
+            </div>
+            <p>{errors?.minLifespan || errors?.maxLifespan}</p>
 
-            <label htmlFor=""></label>
-            <input type="text" id='' />
+            <label htmlFor="temperaments">Dog temperaments</label>
+            <select name="temperaments" id="temperaments" multiple>
+                {allTemps.map((temper, i) => {
+                    return <option key={i} value={temper}>{temper}</option>
+                })}
+            </select>
+            <p>{errors?.temperaments}</p>
+
+            {/* <label htmlFor="image">Seleccione una imagen</label>
+            <input type="file" name='image' id='image' /> */}
 
             <button type='submit'>Submit dog</button>
         </form>
