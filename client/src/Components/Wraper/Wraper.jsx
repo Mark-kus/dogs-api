a177f1e5a32eaed87b5e4a4bb56b8029fc95bb76
Mark-kus <tignanellimarco@gmail.com>
2023-04-01 10:52:07 -0300

    pagination

--- client/src/Components/Wraper/Wraper.jsx
+++ client/src/Components/Wraper/Wraper.jsx
@@ -3,22 +3,53 @@ import getAllDogs from '../../Redux/actions/dogs/getAllDogs.js'
 
 import Card from '../Card/Card';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Wraper() {
-    const allDogs = useSelector(state => state.allDogs);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getAllDogs());
     }, []);
+    const allDogs = useSelector(state => state.allDogs);
+
+    // Paginación
+    const itemsPerPage = 8;
+    const dogsQty = allDogs.length;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [shownDogs, setShownDogs] = useState([...allDogs].splice(0, itemsPerPage));
+
+    const prevHandler = () => {
+        const prevPage = currentPage - 1;
+        if (currentPage === 1) return;
+        setShownDogs([...allDogs].splice((prevPage - 1) * itemsPerPage, itemsPerPage));
+        setCurrentPage(prevPage);
+    }
+
+    const nextHandler = () => {
+        const firstIndex = (currentPage + 1) * itemsPerPage;
+        if (firstIndex > dogsQty) return;
+        setShownDogs([...allDogs].splice(firstIndex, itemsPerPage));
+        setCurrentPage(currentPage + 1);
+    }
+    // Fin de paginación
 
     return (
-        <section>
-            {allDogs.map(dog => <Card
-                key={dog.id}
-                dog={dog} />)}
-        </section>
+        <div className={styles.container}>
+
+            <section>
+                {shownDogs.map(dog => <Card
+                    key={dog.id}
+                    dog={dog} />)}
+
+            </section>
+            <div>
+                <button onClick={prevHandler}>Prev</button>
+                <h4>Página {currentPage}</h4>
+                <button onClick={nextHandler}>Next</button>
+            </div>
+
+        </div>
     )
 }
