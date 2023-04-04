fd5a5e62f26e9c6fdd7e39227d04bd58ce8b594b
Mark-kus <tignanellimarco@gmail.com>
2023-04-04 12:09:07 -0300

    Landing base

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 export default function Landing() {
     return (
-        <>
+        <div className={styles.container}>
         <Link className={styles.link} to='/dogs'>Go see dogs</Link>
-        </>
+        </div>
     )
 }
