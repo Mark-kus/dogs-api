9733f932c915ba27eb3c9eb705387ed35c6c2ddb
Mark-kus <tignanellimarco@gmail.com>
2023-04-04 12:33:50 -0300

    footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import styles from './Footer.module.css';
+import linkedin from '../../linkedin-logo.png';
+import github from '../../github.png';
 
 export default function Footer() {
     return (
         <footer>
-            <h1>Hola, soy un footer</h1>
+            <h4>Created by Mark-kus <br /> &copy;2023 All rights reserved</h4>
+            <div>
+                <a target='_blanck' href="https://www.linkedin.com/in/marco-tignanelli-34871a217/">
+                    <img src={linkedin} />
+                </a>
+                <a target='_blanck' href="https://github.com/Mark-kus">
+                    <img src={github} />
+                </a>
+            </div>
         </footer>
     )
 }
