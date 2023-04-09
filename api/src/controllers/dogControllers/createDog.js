7f843e47a86c31da3f28e02653e1c32edb46a853
Mark-kus <tignanellimarco@gmail.com>
2023-04-09 15:50:21 -0300

    create dogs and associate temps

--- api/src/controllers/dogControllers/createDog.js
+++ api/src/controllers/dogControllers/createDog.js
@@ -2,16 +2,23 @@ const { Dog, Temperament } = require('../../db/db.js');
 
 module.exports = async (name, image, height, weight, lifespan, temperament) => {
     const newDog = await Dog.create({
-        weight,
-        height,
-        name,
-        lifespan,
-        image,
+        weight: weight,
+        height: height,
+        name: name,
+        lifespan: lifespan,
+        image: image,
     });
-    temperament.map(async (temp) => {
-        const temper = await Temperament.findOne({ where: { name: temp } });
+    await temperament.map(async (temp) => {
+        const searchTemp = temp.trim();
+        const temper = await Temperament.findOne({ where: { name: searchTemp } });
         newDog.addTemperament(temper);
     })
 
-    return newDog;
+    return {
+        weigth: newDog.weigth,
+        heigth: newDog.heigth,
+        lifespan: newDog.lifespan,
+        name: newDog.name,
+        id: newDog.id,
+    };
 }
--- api/src/db/db.js
+++ api/src/db/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs` || DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -34,8 +34,8 @@ const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Dog.belongsToMany(Temperament, { through: 'DogTemp' });
-Temperament.belongsToMany(Dog, { through: 'DogTemp' });
+Dog.belongsToMany(Temperament, { through: 'DogTemp', timestamps: false });
+Temperament.belongsToMany(Dog, { through: 'DogTemp', timestamps: false });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
