1a1922fa4b19ac65c9377700a08450dcd51e9c7e
Mark-kus <tignanellimarco@gmail.com>
2023-04-09 15:50:45 -0300

    clean dogs db helper

---
+++ api/src/helpers/cleanArrayDB.js
@@ -0,0 +1,13 @@
+module.exports = (array) =>
+    array.map(elem => {
+        return {
+            weight: elem.weight,
+            height: elem.height,
+            id: elem.id,
+            name: elem.name,
+            lifespan: elem.lifespan,
+            image: elem.image,
+            temperament: elem.temperaments.map(temp => temp.name).join(', '),
+            created: elem.created,
+        }
+    });
