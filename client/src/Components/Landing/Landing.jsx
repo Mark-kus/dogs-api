9bf06784069193fabd3152f62ebc08af0581ea45
Marco <tignanellimarco@gmail.com>
2023-04-10 21:38:43 -0300

    Landing styling

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -5,7 +5,19 @@ import { Link } from 'react-router-dom';
 export default function Landing() {
     return (
         <div className={styles.container}>
-        <Link className={styles.link} to='/dogs'>Go see dogs</Link>
+            <div className={styles.landingContainer}>
+                <div>
+                    <span>
+                        <h1>DOG WEB PAGE</h1>
+                        <h3>The place where all dog breeds are known</h3>
+                            <p>Search information about every breed</p>
+                            <p>Look for your next dog pet</p>
+                            <p>Add your own</p>
+                    </span>
+                    <Link className={styles.link} to='/dogs'>WOOF</Link>
+                </div>
+                <img src="https://images.unsplash.com/photo-1621371236495-1520d8dc72a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJyb3duJTIwZG9nfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="Dog example" />
+            </div>
         </div>
     )
 }
