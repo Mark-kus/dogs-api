04d9673bad9d2a6f133f53f2416bd05438994690
Marco <tignanellimarco@gmail.com>
2023-04-11 01:07:19 -0300

    finish request on error

--- api/src/middlewares/validate.js
+++ api/src/middlewares/validate.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
     const { name, image, height, weight, lifespan, temperament } = req.body;
-    if (![name, image, height, weight, lifespan, temperament].every(Boolean)) res.status(400).json({ msg: "Faltan datos" });
+    if (![name, image, height, weight, lifespan, temperament].every(Boolean)) {
+        res.status(400).json({ msg: "Faltan datos" });
+        return;
+    }
     next();
 }
