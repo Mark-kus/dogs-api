75f664eb10d42067588f7b6eb16ee5620a306e5d
Marco <tignanellimarco@gmail.com>
2023-04-11 00:01:03 -0300

    NaN weight shown fixed

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,9 +1,3 @@
-// Cuando se le hace click a una Card deberá redirigir al detalle de esa raza específica.
-// Botones/Opciones para filtrar por temperamentos,
-// y por si su origen es de la API o de la base de datos (creados por nosotros desde el formulario).
-// Botones/Opciones para ordenar tanto ascendentemente como descendentemente las razas de perros por orden alfabético y por peso.
-// Paginado: el listado de razas de perros se hará por partes.
-
 import styles from './Card.module.css';
 
 import { Link } from 'react-router-dom';
@@ -22,7 +16,7 @@ export default function Card({ dog }) {
             <div>
                 {dog.temperament ? dog.temperament : 'These dog has no temperament assigned'}
                 <br />
-                {dog.weight} kg
+                {!dog.weight.includes('NaN') ? `${dog.weight} kg` : 'Sin peso indicado'}
             </div>
         </article>
     )
