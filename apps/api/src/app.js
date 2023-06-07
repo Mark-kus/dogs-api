476997768bf8dc7799d8c39654e0f17e23e2b229
Marco <tignanellimarco@gmail.com>
2023-06-06 21:12:56 -0300

    deploy fix?

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -14,7 +14,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use((req, res, next) => {            // Esto debe ser reemplazado por * para el deploy
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
